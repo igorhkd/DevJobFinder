81edd9177b11aef1daad93ce3bd4dfa5134b0365
unknown <igor.santanahkd@gmail.com>
2024-07-06 14:39:21 -0300

    feat: configuracao inicial do storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,28 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx|mdx)"],
+
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+  ],
+
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
+  },
+};
+export default config;
